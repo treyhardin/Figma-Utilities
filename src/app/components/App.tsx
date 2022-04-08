cb12f1a1e2b9ddec7847fc481c6389c79a1591b7
Trey Hardin <treyhardindesign@gmail.com>
2022-04-08 11:56:34 -0400

    Image fill mode

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import '../styles/ui.css';
+import 'figma-plugin-ds/dist/figma-plugin-ds.css';
+import {selectMenu, disclosure} from 'figma-plugin-ds';
 
 declare function require(path: string): any;
 
 const App = ({}) => {
     const textbox = React.useRef<HTMLInputElement>(undefined);
+    const imageFillMode = React.useRef<HTMLSelectElement>(undefined);
 
     const countRef = React.useCallback((element: HTMLInputElement) => {
         if (element) element.value = '5';
@@ -16,8 +19,13 @@ const App = ({}) => {
         parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
     };
 
-    const onCancel = () => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
+    const onUpdateImageFill = () => {
+        const fillMode = imageFillMode.current.value;
+        parent.postMessage({pluginMessage: {type: 'set-image-fill-mode', fillMode}}, '*');
+    };
+
+    const onCreateTextStyles = () => {
+        parent.postMessage({pluginMessage: {type: 'create-text-styles'}}, '*');
     };
 
     React.useEffect(() => {
@@ -28,19 +36,36 @@ const App = ({}) => {
                 console.log(`Figma Says: ${message}`);
             }
         };
+
+        //initialize all select menus
+        selectMenu.init();
     }, []);
 
     return (
         <div>
-            <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
-            <p>
-                Count: <input ref={countRef} />
-            </p>
-            <button id="create" onClick={onCreate}>
-                Create
-            </button>
-            <button onClick={onCancel}>Cancel</button>
+            <h3 className="type type--large">Images</h3>
+            <div className="option-set">
+                <div className="label label-heading">Fill Mode</div>
+                <div className="label">Set Image Fill Mode of Selection.</div>
+                <select id="uniqueId" className="select-menu" ref={imageFillMode} onChange={onUpdateImageFill}>
+                    {/* <option>Select Fill Mode</option> */}
+                    <option value="FILL">Fill</option>
+                    <option value="CROP">Crop</option>
+                    <option value="FIT">Fit</option>
+                    <option value="TILE">Tile</option>
+                </select>
+                <button className="button button--secondary" onClick={onUpdateImageFill}>
+                    Set Fill Mode
+                </button>
+            </div>
+            <div className="option-set">
+                <h3 className="type type--large">Typography</h3>
+                <div className="label label-heading">Try Creating Font Styles</div>
+                <div className="label">Create styles from text without styles set.</div>
+                <button className="button button--secondary" onClick={onCreateTextStyles}>
+                    Create Font Styles
+                </button>
+            </div>
         </div>
     );
 };
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,4 +1,4 @@
-figma.showUI(__html__);
+figma.showUI(__html__, {width: 300, height: 400});
 
 figma.ui.onmessage = (msg) => {
     if (msg.type === 'create-rectangles') {
@@ -22,5 +22,47 @@ figma.ui.onmessage = (msg) => {
         });
     }
 
-    figma.closePlugin();
+    if (msg.type === 'set-image-fill-mode') {
+        function setFillMode(node, fillMode) {
+            const newFills = [];
+            for (const paint of node.fills) {
+                if (paint.type === 'IMAGE') {
+                    // Get the (encoded) bytes for this image.
+                    const clonedPaint = JSON.parse(JSON.stringify(paint));
+                    clonedPaint.scaleMode = fillMode;
+                    newFills.push(clonedPaint);
+
+                    node.fills = newFills;
+                }
+            }
+        }
+
+        for (const node of figma.currentPage.selection) {
+            setFillMode(node, msg.fillMode);
+        }
+    }
+
+    if (msg.type === 'create-text-styles') {
+        function createTextStyles(nodes) {
+            const textNodes = [];
+            let textNodesBySize = [];
+
+            // Get Text Elements
+            nodes.map((node) => {
+                if (node.type === 'TEXT') {
+                    textNodes.push(node);
+                }
+            });
+
+            // textNodesBySize = textNodes.sort(function sortBySize(a, b) {
+            //     a.fontSize > b.fontSize
+            // })
+
+            console.log(textNodesBySize);
+        }
+
+        createTextStyles(figma.currentPage.selection);
+    }
+
+    // figma.closePlugin();
 };
